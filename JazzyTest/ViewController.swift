c477dae020d7ee1c0928fa4d4d758f8a98172f21
phillipiLino <phillipi.lino@hotmail.com>
2020-07-23 12:45:42 -0300

    First commit

---
+++ JazzyTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  JazzyTest
+//
+//  Created by Phillipi Unger Lino on 23/07/20.
+//  Copyright © 2020 Phillipi Unger Lino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    /// View Did load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
